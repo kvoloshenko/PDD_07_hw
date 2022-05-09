9baf6a46268dd25a8c3c595c8a8751a20da4b8b5
Konstantin Volosheko <kvoloshenko@gmail.com>
2022-05-09 23:36:39 +0300

    added project

--- bank/bank_data.py
+++ bank/bank_data.py
@@ -14,6 +14,7 @@ all_data['account'] = account
 all_data['purchases'] = purchases
 print(f'all_data={all_data}')
 
+"""
 jsonStr = json.dumps(all_data)
 print(f'jsonStr={jsonStr}')
 
@@ -24,3 +25,17 @@ loaded_account = loaded_all_data ['account']
 print(type(loaded_account), f' loaded_account={loaded_account}')
 loaded_purchases = loaded_all_data ['purchases']
 print(type(loaded_purchases), f' loaded_purchases={loaded_purchases}')
+"""
+
+# Сохранение в файл
+with open('bank_data.json', 'w') as f:
+    json.dump(all_data, f)
+
+
+with open('bank_data.json', 'r') as f:
+    loaded_all_data = json.load(f)
+    print(type(loaded_all_data), f' loaded_all_data={loaded_all_data}')
+    loaded_account = loaded_all_data['account']
+    print(type(loaded_account), f' loaded_account={loaded_account}')
+    loaded_purchases = loaded_all_data['purchases']
+    print(type(loaded_purchases), f' loaded_purchases={loaded_purchases}')
