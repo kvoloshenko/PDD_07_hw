4ff609c149a4e0e108f6f4bf9ea6378f61e448ca
Konstantin Volosheko <kvoloshenko@gmail.com>
2022-05-10 06:27:38 +0300

    added tests for bank_data.py

--- bank/bank_data.py
+++ bank/bank_data.py
@@ -1,41 +1,17 @@
 import json
+import os
 
-all_data = {}
-account = 21
-purchases = []
-purchase_1 = 'aaa 1'
-sum_1 = 10
-purchases.append({'purchase': purchase_1, 'sum': sum_1})
-purchase_2 = 'bbb 2'
-sum_2 = 20
-purchases.append({'purchase': purchase_2, 'sum': sum_2})
-print(f'purchases={purchases}')
-all_data['account'] = account
-all_data['purchases'] = purchases
-print(f'all_data={all_data}')
+def data_save(all_data):
+    # print(f'all_data={all_data}')
+    # Сохранение в файл
+    with open('bank_data.json', 'w', encoding='utf8') as f:
+        json.dump(all_data, f)
 
-"""
-jsonStr = json.dumps(all_data)
-print(f'jsonStr={jsonStr}')
-
-loaded_all_data = json.loads(jsonStr)
-
-print(type(loaded_all_data),f' loaded_all_data={loaded_all_data}')
-loaded_account = loaded_all_data ['account']
-print(type(loaded_account), f' loaded_account={loaded_account}')
-loaded_purchases = loaded_all_data ['purchases']
-print(type(loaded_purchases), f' loaded_purchases={loaded_purchases}')
-"""
-
-# Сохранение в файл
-with open('bank_data.json', 'w') as f:
-    json.dump(all_data, f)
-
-
-with open('bank_data.json', 'r') as f:
-    loaded_all_data = json.load(f)
-    print(type(loaded_all_data), f' loaded_all_data={loaded_all_data}')
-    loaded_account = loaded_all_data['account']
-    print(type(loaded_account), f' loaded_account={loaded_account}')
-    loaded_purchases = loaded_all_data['purchases']
-    print(type(loaded_purchases), f' loaded_purchases={loaded_purchases}')
+def data_read():
+    if os.path.exists('bank_data.json'):
+        with open('bank_data.json', 'r', encoding = 'utf8') as f:
+            loaded_all_data = json.load(f)
+        return loaded_all_data
+    else:
+        all_data = {}
+        return all_data
--- test_filemanager.py
+++ test_filemanager.py
@@ -1,10 +1,14 @@
 import os_tools.tools as f
 import os
 import shutil
+import bank.bank_data as bd
+import json
+
 
 # тесты для "чистых" функций
 def test_about():
     assert f.about() == '(c) Konstantin Voloshenko'
+
 
 # тесты для "грязных" функций
 def test_create_dir():
@@ -33,6 +37,7 @@ def test_copy_dir():
     os.rmdir(dir_name)
     os.rmdir(dir_new)
 
+
 def test_info_dir():
     cur_dir = os.getcwd()
     dir_name = 'test_info_dir'
@@ -46,14 +51,60 @@ def test_info_dir():
     assert f.info_dir('files') == ['test_file_1', 'test_file_2']
     assert f.info_dir('all') == ['test_dir_1', 'test_dir_2', 'test_file_1', 'test_file_2']
     os.chdir(cur_dir)
-    #os.rmdir(dir_name)
+    # os.rmdir(dir_name)
     shutil.rmtree(dir_name, ignore_errors=True)
+
 
 def test_info_os():
     assert len(f.info_os()) > 1
 
 
+def test_data_save():
+    file_name = 'bank_data.json'
+    all_data = {}
+    account = 21
+    purchases = []
+    purchase_1 = 'покупка 1'
+    sum_1 = 10
+    purchases.append({'purchase': purchase_1, 'sum': sum_1})
+    purchase_2 = 'bbb 2'
+    sum_2 = 20
+    purchases.append({'purchase': purchase_2, 'sum': sum_2})
+    # print(f'purchases={purchases}')
+    all_data = {}
+    all_data['account'] = account
+    all_data['purchases'] = purchases
+    bd.data_save(all_data)
+    assert os.path.exists(file_name)
+    assert os.path.isfile(file_name)
+    with open(file_name, 'r', encoding='utf8') as f:
+        loaded_all_data = json.load(f)
+        assert loaded_all_data == all_data
+        loaded_account = loaded_all_data['account']
+        assert account == loaded_account
+        # print(type(loaded_account), f' loaded_account={loaded_account}')
+        loaded_purchases = loaded_all_data['purchases']
+        # print(type(loaded_purchases), f' loaded_purchases={loaded_purchases}')
+        assert purchases == loaded_purchases
+    os.remove(file_name)
 
-
-
+def test_data_read():
+    assert bd.data_read() == {}
+    file_name = 'bank_data.json'
+    all_data = {}
+    account = 21
+    purchases = []
+    purchase_1 = 'покупка 1'
+    sum_1 = 10
+    purchases.append({'purchase': purchase_1, 'sum': sum_1})
+    purchase_2 = 'bbb 2'
+    sum_2 = 20
+    purchases.append({'purchase': purchase_2, 'sum': sum_2})
+    all_data = {}
+    all_data['account'] = account
+    all_data['purchases'] = purchases
+    with open(file_name, 'w', encoding='utf8') as f:
+        json.dump(all_data, f)
+    assert bd.data_read() == all_data
+    os.remove(file_name)
 
